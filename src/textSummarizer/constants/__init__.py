998c7eee35cd80624327acdc349ba4f63a2de61b
AlrikF <alrik.fernandes@gmail.com>
2023-09-08 17:53:04 -0700

    data-ingestion-pieline completed

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 # These paths won't change 
-CONFIG_FILE_PATH = Path("./configssss/config.yaml")
+CONFIG_FILE_PATH = Path("./config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
