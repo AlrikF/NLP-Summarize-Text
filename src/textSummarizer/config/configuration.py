460ed0e2157d4d4efb6636f029196fea1e06deb3
AlrikF <alrik.fernandes@gmail.com>
2023-09-21 13:09:34 -0700

    Data Ingestion Completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -8,10 +8,7 @@ from src.textSummarizer.entity import (DataIngestionConfig,
 
 
 class ConfigurationManager:
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
+    def __init__( self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
 
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
